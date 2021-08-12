709de1463f42aabe13baf832017f25a703aa9d2e
Joey Fang <josudoey@gmail.com>
2021-08-12 16:20:31 +0800

    docs: add event ref link

--- watcher.go
+++ watcher.go
@@ -11,6 +11,7 @@ import (
 
 // GetFirstPod returns a pod matching the namespace and label selector
 // and the number of all pods that match the label selector.
+// see https://github.com/kubernetes/apiserver/blob/92392ef22153d75b3645b0ae339f89c12767fb52/pkg/endpoints/handlers/watch.go
 func GetPodWatcher(ctx context.Context, client coreclient.PodsGetter, namespace string, selector string, podList *v1.PodList) (watch.Interface, error) {
 	options := metav1.ListOptions{LabelSelector: selector}
 	options.ResourceVersion = podList.ResourceVersion
